962420af34b0f7fc4fe6399d2b4366a0d5157327
Göran Krampe <goran.krampe@gmail.com>
2020-08-04 22:16:19 +0200

    Trivial changes/updates

--- code/build/android-native/install.sh
+++ code/build/android-native/install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-mkdir ../../lib/static/android-native/armeabi-v7a
-mkdir ../../lib/static/android-native/arm64-v8a
-mkdir ../../lib/static/android-native/x86
-mkdir ../../lib/static/android-native/x86_64
+mkdir -p ../../lib/static/android-native/armeabi-v7a
+mkdir -p ../../lib/static/android-native/arm64-v8a
+mkdir -p ../../lib/static/android-native/x86
+mkdir -p ../../lib/static/android-native/x86_64
 cp ./obj/local/armeabi-v7a/liborx*.a ../../lib/static/android-native/armeabi-v7a
 cp ./obj/local/arm64-v8a/liborx*.a ../../lib/static/android-native/arm64-v8a
 cp ./obj/local/x86/liborx*.a ../../lib/static/android-native/x86
